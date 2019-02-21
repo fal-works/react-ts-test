4afd2f9588493665cdabf8743cf25869cba49655
FAL <falworks.contact@gmail.com>
2019-02-22 03:50:29 +0900

    Remove prop-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,7 +40,8 @@ module.exports = {
 
         // react
         "react/jsx-uses-vars": "error",
-        "react/jsx-uses-react": "error",   
+        "react/jsx-uses-react": "error",
+        "react/prop-types": "off",
 
         // node
         "node/no-missing-import": "error",
@@ -67,5 +68,24 @@ module.exports = {
         "node": {
             "tryExtensions": [".ts", ".js", ".json", ".node", ".tsx"]
         },
+        "react": {
+            "createClass": "createReactClass", // Regex for Component Factory to use,
+            // default to "createReactClass"
+            "pragma": "React",  // Pragma to use, default to "React"
+            "version": "detect", // React version. "detect" automatically picks the version you have installed.
+            // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+            "flowVersion": "0.53" // Flow version
+        },
+        "propWrapperFunctions": [
+            // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+            "forbidExtraProps",
+            { "property": "freeze", "object": "Object" },
+            { "property": "myFavoriteWrapper" }
+        ],
+        "linkComponents": [
+            // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+            "Hyperlink",
+            { "name": "Link", "linkAttribute": "to" }
+        ]
     },
 };
